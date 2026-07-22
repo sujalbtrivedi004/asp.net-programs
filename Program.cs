fafb67134bba2da5c96d85f7f6fad387317cc6d8
sujalbtrivedi004 <sujaltrivedib@gmail.com>
2026-07-22 11:35:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum = 0, digit;
+            Console.Write("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            while (number != 0)
+            {
+                digit = number % 10; // Get the last digit
+                sum = sum + digit; // Add the digit to sum
+                number = number / 10; // Remove the last digit
+            }
+            Console.WriteLine("Sum of digits = " + sum);
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
