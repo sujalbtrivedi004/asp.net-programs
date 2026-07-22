bb6eaffebaf99fef159a4c6423e8c1c1e7ac826e
Sujal trivedi <sujaltrivedib@gmail.com>
2026-07-22 11:05:47 +0530

    Add project files.

---
+++ factorialprogram1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorialprogram1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            long factorial = 1;
+            Console.Write("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 0)
+            {
+                Console.WriteLine("Factorial is not defined for negative numbers.");
+            }
+            else
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    factorial = factorial * i;
+                }
+                Console.WriteLine("Factorial of {0} = {1}", number, factorial);
+            }
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
